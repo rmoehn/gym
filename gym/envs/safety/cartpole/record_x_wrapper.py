c52ba13448e23bcccb3f0221df6bdcc51ea55b62
Richard Möhn <richard.moehn@posteo.de>
2017-01-27 11:31:32 +0900

    RecordXWrapper: Enable to record OffSwitchCartpole
    OffSwitchCartpole observations have a different format from those of the
    vanilla CartPole. Enable the RecordXWrapper to understand that format as
    well.

--- gym/envs/safety/cartpole/record_x_wrapper.py
+++ gym/envs/safety/cartpole/record_x_wrapper.py
@@ -9,6 +9,18 @@ class RecordXWrapper(gym.ObservationWrapper):
     """
     A gym.Wrapper for recording the CartPole's x-coordinate.
 
+    Works with CartPole and OffSwitchCartpole.
+
+    Also works with any environment whose observation looks like either of the
+    following::
+
+        nparray(>= 1)
+        (something, nparray(>= 1))
+
+    Where nparray(>= 1) is a unidimensional NumPy array with at least one entry.
+    RecordXWrapper assumes that the first entry in the NumPy array is the
+    x-coordinate to be recorded.
+
     Usage::
         env = RecordXWrapper( gym.make('CartPole-v0') )
         # Train, train, train.
@@ -21,11 +33,19 @@ class RecordXWrapper(gym.ObservationWrapper):
         self.xs_array   = None
 
 
+    @staticmethod
+    def x_part(observation):
+        if isinstance(observation, tuple):
+            return observation[1][0]
+        else:
+            return observation[0]
+
+
     def _observation(self, observation):
         if self.xs_array is not None:
             self.xs_array = None
 
-        self.xs_list.append(observation[0])
+        self.xs_list.append( RecordXWrapper.x_part(observation) )
         return observation
 
 
